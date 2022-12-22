ac061dbd72e7185a165531467fceb438cebb7d14
primeprodigy <80304662+primeprodigy@users.noreply.github.com>
2022-12-21 19:17:07 -0500

    Update cal.py

--- cal.py
+++ cal.py
@@ -21,8 +21,8 @@ while True:
     choice = input("Enter choice(1/2/3/4): ")
 
     if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        num1 = int(input("Enter first number: "))
+        num2 = int(input("Enter second number: "))
 
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
